0ce84ba2fd8d2410f1863f2dcec628d45f6a3e05
Alexander Sevirinov <asevirinov@gmail.com>
2018-02-05 14:46:48 +0300

    add form example with validation

--- app/js/app.js
+++ app/js/app.js
@@ -1,11 +1,81 @@
-"use strict";
+(() => {
+  'use strict';
 
-$(() => {
-  // Плавный скроллинг к элементу
-  $('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(e => {
-    $('html,body').stop().animate({
-      scrollTop: $(e.currentTarget.hash).offset().top
-    }, 1000);
-    e.preventDefault();
+  $(() => { // jQuery ready
+
+    /* Smooth scroll */
+    $('a[href*="#"]').
+        not('[href="#"]').
+        not('[href="#0"]').
+        not('[data-toggle="collapse"]').
+        not('[role="tab"]').
+        click(e => {
+          $('html,body').stop().animate({
+            scrollTop: $(e.currentTarget.hash).offset().top
+          }, 1000);
+          e.preventDefault();
+        });
+
+    /* Phone mask */
+    $('input[type="tel"]').inputmask({
+      alias: 'phoneru',
+      clearIncomplete: true
+    });
+
+    /* Validation form */
+    let validation = form => {
+      let fields = form.find('input, select'),
+          check = [];
+
+      $.each(fields, (_, item) => {
+        if ($(item).is(':required') && !$(item).is(':disabled')) {
+          check.push(item.checkValidity());
+        }
+      });
+      form.addClass('was-validated');
+      let isValid = item => item === true;
+      if (check.every(isValid)) {
+        form.removeClass('was-validated');
+      }
+      return check.every(isValid);
+    };
+
+    /* Form processing */
+    $(document).on('submit', 'form', e => {
+      let $form = $(e.currentTarget),
+          $submitBtn = $form.find('[type="submit"]'),
+          formData = {};
+
+      $.each($form.serializeArray(), (_, item) => {
+        if (item.name !== 'agree') {
+          formData[item.name] = item.value;
+        }
+      });
+
+      if (validation($form)) {
+        $submitBtn.addClass('disabled');
+
+        $.post('./mail.php', formData).done(json => {
+          if (json.status === 'success') {
+            setTimeout(() => {
+              alert('OK!');
+            }, 300);
+          } else {
+            alert(
+                'Что-то пошло не так! Обновите страницу и попробуйте ещё раз!');
+          }
+        }, 'json');
+
+        setTimeout(() => {
+          $submitBtn.removeClass('disabled');
+          $form[0].reset();
+        }, 1000);
+
+        console.log(formData); // debug only
+      }
+
+      e.preventDefault();
+    });
   });
-});
+
+})();

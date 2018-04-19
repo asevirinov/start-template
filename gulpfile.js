1f705ce46ed3edbbc1d4c8ada0084c0f4c3e8e21
Alexander Sevirinov <asevirinov@gmail.com>
2018-04-19 10:48:33 +0300

    fix path

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,42 @@
+const ENV = {
+  devMode: true,
+  slimJquery: false,
+  name: {},
+  bootstrap: [
+    'app/libs/popper.js/dist/umd/popper.js',
+    'app/libs/bootstrap/js/dist/util.js',
+    'app/libs/bootstrap/js/dist/alert.js',
+    'app/libs/bootstrap/js/dist/button.js',
+    'app/libs/bootstrap/js/dist/carousel.js',
+    'app/libs/bootstrap/js/dist/collapse.js',
+    'app/libs/bootstrap/js/dist/dropdown.js',
+    'app/libs/bootstrap/js/dist/modal.js',
+    'app/libs/bootstrap/js/dist/scrollspy.js',
+    'app/libs/bootstrap/js/dist/tab.js',
+    'app/libs/bootstrap/js/dist/tooltip.js',
+    'app/libs/bootstrap/js/dist/popover.js',
+  ],
+  inputmask: [
+    'app/libs/inputmask/dist/inputmask/inputmask.js',
+    'app/libs/inputmask/dist/inputmask/inputmask.extensions.js',
+    'app/libs/inputmask/dist/inputmask/inputmask.numeric.extensions.js',
+    'app/libs/inputmask/dist/inputmask/inputmask.date.extensions.js',
+    'app/libs/inputmask/dist/inputmask/inputmask.phone.extensions.js',
+    'app/libs/inputmask/dist/inputmask/jquery.inputmask.js',
+    'app/libs/inputmask/dist/inputmask/phone-codes/phone.js',
+    'app/libs/inputmask/dist/inputmask/phone-codes/phone-be.js',
+    'app/libs/inputmask/dist/inputmask/phone-codes/phone-nl.js',
+    'app/libs/inputmask/dist/inputmask/phone-codes/phone-uk.js',
+    'app/libs/inputmask/dist/inputmask/phone-codes/phone-ru.js',
+  ],
+  ftp: {
+    host: '',
+    user: '',
+    pass: '',
+    path: ''
+  }
+};
+
 const browserSync = require('browser-sync'),
     cssSort = require('css-declaration-sorter'),
     del = require('del'),
@@ -16,65 +55,39 @@ const browserSync = require('browser-sync'),
     uglify = require('gulp-uglify'),
     ftp = require('vinyl-ftp');
 
-const devMode = true;
-
 gulp.task('app-js', () => {
   return gulp.src(['app/js/app.js']).
       pipe(babel({
-        presets: ['env']
+        presets: ['env'],
       })).
       pipe(concat('app.min.js')).
-      pipe(gulpif(!devMode, uglify())).
+      pipe(gulpif(!ENV.devMode, uglify())).
       pipe(gulp.dest('app/js'));
 });
 
 gulp.task('bootstrap-js', () => {
-  return gulp.src([
-    // popper.js only top!
-    'app/libs/popper.js/dist/umd/popper.js',
-    'app/libs/bootstrap/js/dist/util.js',
-    'app/libs/bootstrap/js/dist/alert.js',
-    'app/libs/bootstrap/js/dist/button.js',
-    'app/libs/bootstrap/js/dist/carousel.js',
-    'app/libs/bootstrap/js/dist/collapse.js',
-    'app/libs/bootstrap/js/dist/dropdown.js',
-    'app/libs/bootstrap/js/dist/modal.js',
-    'app/libs/bootstrap/js/dist/scrollspy.js',
-    'app/libs/bootstrap/js/dist/tab.js',
-    'app/libs/bootstrap/js/dist/tooltip.js',
-    'app/libs/bootstrap/js/dist/popover.js'
-  ]).
+  return gulp.src(ENV.bootstrap).
       pipe(concat('bootstrap.js')).
       pipe(gulp.dest('app/libs/bootstrap/js'));
 });
 
 gulp.task('inputmask-js', () => {
-  return gulp.src([
-    'app/libs/inputmask/dist/inputmask/inputmask.js',
-    'app/libs/inputmask/dist/inputmask/inputmask.extensions.js',
-    'app/libs/inputmask/dist/inputmask/inputmask.numeric.extensions.js',
-    'app/libs/inputmask/dist/inputmask/inputmask.date.extensions.js',
-    'app/libs/inputmask/dist/inputmask/inputmask.phone.extensions.js',
-    'app/libs/inputmask/dist/inputmask/jquery.inputmask.js',
-    'app/libs/inputmask/dist/inputmask/phone-codes/phone.js',
-    'app/libs/inputmask/dist/inputmask/phone-codes/phone-be.js',
-    'app/libs/inputmask/dist/inputmask/phone-codes/phone-nl.js',
-    'app/libs/inputmask/dist/inputmask/phone-codes/phone-uk.js',
-    'app/libs/inputmask/dist/inputmask/phone-codes/phone-ru.js'
-  ]).
+  return gulp.src(ENV.inputmask).
       pipe(concat('inputmask.js')).
       pipe(gulp.dest('app/libs/inputmask/dist'));
 });
 
 gulp.task('js', ['app-js', 'bootstrap-js', 'inputmask-js'], () => {
+  let jQueryVersion = slimJquery
+      ? 'app/libs/jquery/dist/jquery.slim.js'
+      : 'app/libs/jquery/dist/jquery.js';
   return gulp.src([
-    'app/libs/jquery/dist/jquery.js',
-    // 'app/libs/jquery/dist/jquery.slim.js',
+    jQueryVersion,
     'app/libs/bootstrap/js/bootstrap.js',
-    'app/libs/inputmask/dist/inputmask.js'
+    'app/libs/inputmask/dist/inputmask.js',
   ]).
       pipe(concat('vendor.min.js')).
-      pipe(gulpif(!devMode, uglify())).
+      pipe(gulpif(!ENV.devMode, uglify())).
       pipe(gulp.dest('app/js')).
       pipe(browserSync.reload({stream: true}));
 });
@@ -82,9 +95,9 @@ gulp.task('js', ['app-js', 'bootstrap-js', 'inputmask-js'], () => {
 gulp.task('browser-sync', () => {
   browserSync({
     server: {
-      baseDir: 'app'
+      baseDir: 'app',
     },
-    notify: false
+    notify: false,
   });
 });
 
@@ -92,9 +105,9 @@ gulp.task('scss', () => {
   return gulp.src('app/scss/**/*.scss').
       pipe(scss({outputStyle: 'expand'}).on('error', notify.onError())).
       pipe(rename({suffix: '.min', prefix: ''})).
-      pipe(autoprefixer(['last 15 versions'])).
+      pipe(autoprefixer(['last 6 versions'])).
       pipe(postCss([cssSort({order: 'smacss'})])).
-      pipe(gulpif(!devMode, cleanCSS())).
+      pipe(gulpif(!ENV.devMode, cleanCSS())).
       pipe(gulp.dest('app/css')).
       pipe(browserSync.reload({stream: true}));
 });
@@ -102,7 +115,7 @@ gulp.task('scss', () => {
 gulp.task('watch', ['scss', 'js', 'browser-sync'], () => {
   gulp.watch([
     'app/scss/**/*.scss',
-    'app/libs/bootstrap/scss/**/*.scss'
+    'app/libs/bootstrap/scss/**/*.scss',
   ], ['scss']);
   gulp.watch(['libs/**/*.js', 'app/js/app.js'], ['js']);
   gulp.watch('app/*.html', browserSync.reload);
@@ -125,19 +138,20 @@ gulp.task('build', ['rename-htaccess', 'removedist', 'imagemin', 'scss', 'js'],
     () => {
       let buildFiles = gulp.src([
         'app/*.html',
-        'app/.htaccess'
+        'app/.htaccess',
+        'app/mail.php',
       ]).pipe(gulp.dest('dist'));
 
       let buildCss = gulp.src([
-        'app/css/*.*'
+        'app/css/*.*',
       ]).pipe(gulp.dest('dist/css'));
 
       let buildJs = gulp.src([
-        'app/js/*.*'
+        'app/js/*.*',
       ]).pipe(gulp.dest('dist/js'));
 
       let buildFonts = gulp.src([
-        'app/fonts/**/*'
+        'app/fonts/**/*',
       ]).pipe(gulp.dest('dist/fonts'));
     });
 
@@ -151,18 +165,18 @@ gulp.task('clearcache', () => {
 
 gulp.task('deploy', () => {
   let conn = ftp.create({
-    host: '',
-    user: '',
-    password: '',
-    parallel: 10
+    host: ENV.ftp.host,
+    user: ENV.ftp.user,
+    password: ENV.ftp.pass,
+    parallel: 10,
   });
 
   let globs = [
     'dist/**',
-    'dist/.htaccess'
+    'dist/.htaccess',
   ];
   return gulp.src(globs, {buffer: false}).
-      pipe(conn.dest('/www/'));
+      pipe(conn.dest(ENV.ftp.path));
 });
 
 gulp.task('default', ['watch']);
